b5dbadc69daee2d1580545c68644d62f32b2834a
NamSangKyu <nam2626@gmail.com>
2021-06-21 14:50:43 +0900

    원넓이 원둘레 문제 완료

--- java/Step01/src/Quest2.java
+++ java/Step01/src/Quest2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Quest2 {
 	/*
@@ -5,7 +6,14 @@ public class Quest2 {
 	 * 반지름에 해당하는 원넓이와 원둘레를 출력
 	 */
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int r;
+		System.out.println("반지름 하나 입력하세요");
+		r = sc.nextInt();
+		double area = r*r*3.1415;
+		System.out.println(area);
+		double roundLength = 2*r*3.1415;
+		System.out.println(roundLength);
 	}
 
 }
