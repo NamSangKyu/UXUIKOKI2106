e78d7f94c739878c9066ec0349843e5314a91da4
NamSangKyu <nam2626@gmail.com>
2021-06-22 14:52:45 +0900

    else 예제

---
+++ java/Step02/src/IfElseEx.java
@@ -0,0 +1,14 @@
+
+public class IfElseEx {
+
+	public static void main(String[] args) {
+		int n = 4; 
+		
+		if(n % 2 == 1)
+			System.out.println("해당 숫자는 홀수 입니다.");
+		else//위에있는 조건들이 전부 거짓일때 실행하는 영역
+			System.out.println("해당 숫자는 짝수 입니다.");
+		
+	}
+
+}
