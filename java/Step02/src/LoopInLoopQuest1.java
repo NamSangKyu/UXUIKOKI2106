e334dd1d3b245d970de598dbf283df7a927a5625
NamSangKyu <nam2626@gmail.com>
2021-06-22 12:39:00 +0900

    반복문 중첩 문제

---
+++ java/Step02/src/LoopInLoopQuest1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LoopInLoopQuest1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 숫자 하나 입력을 받으면 아래와 같이 출력
+		 * 숫자 입력 : 5
+		 *     *
+		 *    **
+		 *   ***
+		 *  ****
+		 * *****
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 입력 > ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String str = "";
+			// 공백 처리
+			for (int j = 0; j < n - i - 1; j++) {
+				str += " ";
+			}
+			//별 처리
+			for(int j=0;j<=i;j++)
+				str += "*";
+			System.out.println(str);
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
