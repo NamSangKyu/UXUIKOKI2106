7c12124ae06c76c910d33d04eca0d75459b4b5a3
NamSangKyu <nam2626@gmail.com>
2021-06-22 17:28:48 +0900

    Switch 예제

---
+++ java/Step02/src/SwitchEx.java
@@ -0,0 +1,26 @@
+
+public class SwitchEx {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		switch(n) {
+		case 4:
+			System.out.println("Four");
+			break;
+		case 3:
+			System.out.println("Three");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		case 1:
+			System.out.println("One");
+			break;
+		default:
+			System.out.println("None");
+			
+		}
+	}
+
+}
