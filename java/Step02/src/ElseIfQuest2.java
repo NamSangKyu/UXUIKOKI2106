921c2093ce0867b1104c1d5cd0e9c2507fb81c7b
NamSangKyu <nam2626@gmail.com>
2021-06-22 15:36:10 +0900

    else if 문제 2

---
+++ java/Step02/src/ElseIfQuest2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ElseIfQuest2 {
+
+	public static void main(String[] args) {
+		//평점을 입력 받으면 해당 평점에 해당하는 등급을 출력
+		/*
+		 * 		평점 입력 : 4.2
+		 * 		4.5   		A+
+		 * 		4.0 ~ 4.4	A
+		 * 		3.5 ~ 3.9	B+
+		 * 		3.0 ~ 3.4	B
+		 * 		2.5 ~ 2.9	C+
+		 * 		2.0	~ 2.4	C
+		 * 		1.5 ~ 1.9	D+
+		 * 		1.0	~ 1.4	D
+		 * 		0.0 ~ 0.9	F
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("평점 입력 : ");
+		double score = sc.nextDouble();
+		
+		
+	}
+
+}
+
+
+
+
