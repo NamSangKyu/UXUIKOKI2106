256d5211da5a9fa52c7a6a6f95eec02cdb95f203
NamSangKyu <nam2626@gmail.com>
2021-06-22 15:13:50 +0900

    else if 예제

---
+++ java/Step02/src/ElseIfEx.java
@@ -0,0 +1,17 @@
+
+public class ElseIfEx {
+
+	public static void main(String[] args) {
+		int n = 6;
+
+		if (n % 2 == 1) {
+			System.out.println("이 숫자는 홀수 입니다.");
+		} else if (n == 0) {
+			System.out.println("이 숫자는 0 입니다.");
+		} else {
+			System.out.println("이 숫자는 짝수 입니다.");
+		}
+		
+	}
+
+}
