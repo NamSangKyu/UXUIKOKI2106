04829e84473f61277d9bd0d62808dd261552c992
NamSangKyu <nam2626@gmail.com>
2021-06-24 15:40:01 +0900

    에어컨 전원 기능 작성

--- java/Step04/src/Aircon.java
+++ java/Step04/src/Aircon.java
@@ -17,7 +17,32 @@ public class Aircon {
 		this.deg = deg;
 		this.timer = timer;
 	}
+	public Aircon() {
+		power = false;
+		temp = 24;
+		windPower = 1;
+	}
+		
+	//에어컨 전원 켜고 끄고 기능, true 전원 On, false 전원 Off
+	void powerOnOff() {
+		power = !power;
+		if(power)
+			System.out.println("에어컨 전원 켜졌습니다.");
+		else
+			System.out.println("에어컨 전원 꺼졌습니다.");
+			
+	}
+	//온도를 올리는 기능
+	
+	//온도를 내리는 기능
 	
 	
 	
 }
+
+
+
+
+
+
+
---
+++ java/Step04/src/AirconMain.java
@@ -0,0 +1,12 @@
+
+public class AirconMain {
+
+	public static void main(String[] args) {
+		Aircon air = new Aircon();
+		air.powerOnOff();
+		air.powerOnOff();
+		air.powerOnOff();
+		air.powerOnOff();
+	}
+
+}
