2d25992c6e144d4e6db6c4ee3581f11d5f98c0c0
NamSangKyu <nam2626@gmail.com>
2021-06-25 10:39:45 +0900

    학생 정보 등록 완료

--- java/StudentProject/src/service/StudentService.java
+++ java/StudentProject/src/service/StudentService.java
@@ -15,10 +15,26 @@ public class StudentService {
 	//학생 데이터 등록
 	//	학생데이터 입력 받아서 배열에 등록
 	public void insertStudent(Scanner sc) {
+		System.out.println("학생정보 등록을 시작합니다..........");
+		if(index == arr.length) {
+			System.out.println("더 이상 저장할 공간이 없습니다.");
+			return;
+		}
 		//학생데이터 입력
+		System.out.print("학번 입력 : ");
+		String studentNo = sc.nextLine();
+		System.out.print("이름 입력 : ");
+		String name = sc.nextLine();
+		System.out.print("학과 입력 : ");
+		String major = sc.nextLine();
+		System.out.print("평점 입력 : ");
+		double score = sc.nextDouble();
+		sc.nextLine();
 		//Student 객체 생성
 		//배열에 저장
+		arr[index] = new StudentVO(studentNo, name, major, score);
 		//index 증가
+		index++;
 	}
 	//학생 데이터 검색
 	//학생 데이터 삭제
