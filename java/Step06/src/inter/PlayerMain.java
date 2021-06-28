c0605ebba80bee2009b89fc9216fe7fe8f8ff6e9
NamSangKyu <nam2626@gmail.com>
2021-06-28 12:41:09 +0900

    인터페이스 예제

--- java/Step06/src/inter/PlayerMain.java
+++ java/Step06/src/inter/PlayerMain.java
@@ -1,13 +1,22 @@
 package inter;
 
 public class PlayerMain {
-
+	
+	public static void player(Player p) {
+		//인터페이스로 형변환 후 실행이 가능한 것은
+		//선언해 놓은 추상메서드를 이용하여 해당객체를 제어를 할수있다.
+		p.play();
+		p.next();
+		p.pre();
+		p.stop();
+	}
+	
 	public static void main(String[] args) {
 		MusicPlayer musicPlayer = new MusicPlayer();
-		musicPlayer.play();
-		musicPlayer.stop();
-		musicPlayer.next();
-		musicPlayer.pre();
+		YoutupePlayer youtupePlayer = new YoutupePlayer();
+		
+		player(youtupePlayer);
+		player(musicPlayer);
 	}
 
 }
