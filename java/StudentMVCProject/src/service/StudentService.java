93c9e813e1112122e7faa5d2febb4eb19463c634
NamSangKyu <nam2626@gmail.com>
2021-06-28 15:08:00 +0900

    학생 정보 등록 완료

--- java/StudentMVCProject/src/service/StudentService.java
+++ java/StudentMVCProject/src/service/StudentService.java
@@ -19,6 +19,28 @@ public class StudentService {
 			instance = new StudentService();
 		return instance;
 	}
+	public void reallocArray() {
+		StudentVO[] temp = arr;
+		arr = new StudentVO[temp.length+5];
+		for(int i=0;i<temp.length;i++)
+			arr[i] = temp[i];
+	}
+	public boolean registerStudent(StudentVO vo) {
+		if(index==arr.length)
+			reallocArray();
+		for(int i=0;i<index;i++) {
+			//동일한 학번이있는지 검사
+			if(arr[i].getStudentNo().equals(vo.getStudentNo()))
+				return false;//학생정보를 등록 못한 경우 false
+		}
+		arr[index] = vo;//받아온 vo 배열에 저장
+		index++;
+		return true;//학생정보를 등록을 한 경우 true;
+	}
 
 	
 }
+
+
+
+
