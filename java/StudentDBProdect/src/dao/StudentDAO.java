5d856fde50461af7de6d9ca1119fcd53eec8a51e
NamSangKyu <nam2626@gmail.com>
2021-07-08 15:46:41 +0900

    DAO 학생정보 등록 완료

--- java/StudentDBProdect/src/dao/StudentDAO.java
+++ java/StudentDBProdect/src/dao/StudentDAO.java
@@ -1,9 +1,16 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import db.DBManager;
-
+import vo.StudentVO;
+/*
+ * DAO 클래스는 Service 클래스로부터 데이터를 받아서
+ * DB에 데이터를 조작, 조회, 결과를 Service클래스로 리턴
+ *  
+ */
 public class StudentDAO {
 	private static StudentDAO instance = new StudentDAO();
 	private Connection conn;
@@ -16,6 +23,30 @@ public class StudentDAO {
 			instance = new StudentDAO();
 		return instance;
 	}
+	
+	//1. 학생정보 등록
+	public int insertStudentVO(StudentVO vo) {
+		String sql = "insert into student values(?,?,?,?)";
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getSno());
+			pstmt.setString(2, vo.getName());
+			pstmt.setInt(3, vo.getMajorNo());
+			pstmt.setDouble(4, vo.getScore());
+			
+			int count= pstmt.executeUpdate();
+			System.out.println(count + "건 적용 완료");
+			return count;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	//2. 학생정보 검색
+	//3. 학생정보 수정
+	//4. 학생정보 삭제
+	//5. 전체 학생정보 출력
 
 }
 
