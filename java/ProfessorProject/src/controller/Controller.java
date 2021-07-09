ad09f4fbf0af63f991ed15d92461e3c0b99f871d
NamSangKyu <nam2626@gmail.com>
2021-07-09 12:49:41 +0900

    교수 담당 프로젝트

---
+++ java/ProfessorProject/src/controller/Controller.java
@@ -0,0 +1,8 @@
+package controller;
+
+
+import java.util.Scanner;
+
+public interface Controller {
+	public void excute(Scanner sc);
+}
---
+++ java/ProfessorProject/src/controller/HandlerMapping.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class HandlerMapping {
+	private static HandlerMapping instance = new HandlerMapping();
+
+	private HandlerMapping() {
+
+	}
+
+	public static HandlerMapping getInstance() {
+		if(instance == null)
+			instance = null;
+		return instance;
+	}
+	
+	public Controller createController(int no) {
+		Controller controller = null;
+		return controller;
+	}
+		
+}
+
+
