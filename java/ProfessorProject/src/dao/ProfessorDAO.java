e8e7bdfad0c2d872a2959b1453e75272483284e6
NamSangKyu <nam2626@gmail.com>
2021-07-09 16:51:25 +0900

    로그인 작업 완료

--- java/ProfessorProject/src/dao/ProfessorDAO.java
+++ java/ProfessorProject/src/dao/ProfessorDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import db.DBManager;
 
@@ -16,10 +19,38 @@ public class ProfessorDAO {
 		return instance;
 	}
 	public boolean login(String no, String name) {
-		return false;
+		String sql = "select * from profersor "
+				+ "where profersor_no like ? and profersor_name like ?";
+		ResultSet rs = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, no);
+			pstmt.setString(2, name);
+			rs = pstmt.executeQuery();
+			result = rs.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null)rs.close();
+				if(pstmt != null)pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
 	}
 	
 }
+
+
+
+
+
+
 
 
 
