f6bef94ca5171c9fffbea60fef014465e354a6f6
NamSangKyu <nam2626@gmail.com>
2021-08-19 10:50:32 +0900

    CORS 처리

--- js/node/08_express_api.js
+++ js/node/08_express_api.js
@@ -6,6 +6,13 @@ const port = 3000;
 app.listen(port,()=>{
     console.log(`서버 오픈 완료, 포트번호 : ${port}`);
 });
+//CORS 오류 처리
+app.all("/*",function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","X-Requested-With");
+    next();
+});
+
 app.get("/sendDate", (req, res) => {
     var now = new Date();
     var data = {};
