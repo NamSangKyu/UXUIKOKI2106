c257481c084b8fc5dd2a09d067e90dcad9906c3d
NamSangKyu <nam2626@gmail.com>
2021-08-27 17:18:09 +0900

    MVC 처리 준비 완료

--- jsp/MemberServletProject/src/main/java/servlet/DispatcherServlet.java
+++ jsp/MemberServletProject/src/main/java/servlet/DispatcherServlet.java
@@ -7,10 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import controller.Controller;
+import controller.HandlerMapping;
+import view.ModelAndView;
+
 /**
  * Servlet implementation class DispatcherServlet
  */
-@WebServlet("/DispatcherServlet")
+@WebServlet("*.action")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,15 +30,30 @@ public class DispatcherServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=utf-8");
+		String[] arr = request.getRequestURI().split("/");
+		String command = arr[arr.length-1];
+		//response.getWriter().print(command);
+		Controller controller = HandlerMapping.getInstance().createController(command);
+		ModelAndView view = null;
+		if(controller != null)
+			view = controller.execute(request, response);
+		
+		if(view == null) return;
+		
+		if(view.isRedirect()) {
+			response.sendRedirect(view.getPath());
+		}else {
+			request.getRequestDispatcher(view.getPath()).forward(request, response);
+		}
+			
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
 		doGet(request, response);
 	}
 
