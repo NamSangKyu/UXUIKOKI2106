5fe12346c0a6bb2a0bf48b374f5005d4f6dcc3d8
NamSangKyu <nam2626@gmail.com>
2021-09-03 11:23:02 +0900

    파일 업로드 서블릿 작성

--- jsp/FileTest/src/main/java/file/FileUploadServlet.java
+++ jsp/FileTest/src/main/java/file/FileUploadServlet.java
@@ -2,6 +2,7 @@ package file;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -50,10 +51,44 @@ public class FileUploadServlet extends HttpServlet {
 		
 		try {
 			List<FileItem> list = upload.parseRequest(request);
-			
+			ArrayList<FileDTO> flist = new ArrayList<FileDTO>();
+			String user = "default";
+			for(int i=0;i<list.size();i++) {
+				FileItem item = list.get(i);
+				//받은 내용 중에 파일이 아니고 type="text,password....." 인경우
+				if(item.isFormField()) {
+					if(item.getFieldName().equals("writer"))
+						user = item.getString(encoding);
+					System.out.println(item.getFieldName() + " : " + item.getString(encoding));
+				}else {
+					//받은 내용이 파일인 경우
+					System.out.println("매개변수 명 : "+item.getFieldName());
+					System.out.println("파일명 : "+item.getName());
+					System.out.println("파일 크기 : "+item.getSize());
+					System.out.println("파일 타입 : "+item.getContentType());
+					if(item.getSize() > 0) {
+						int idx = item.getName().lastIndexOf("\\");
+						if(idx==-1)
+							idx = item.getName().lastIndexOf("/");
+						String fileName = item.getName().substring(idx+1);
+						//파일 경로 완성
+						File uploadFile = new File(root+"\\"+fileName);
+						if(!uploadFile.getParentFile().exists())
+							uploadFile.getParentFile().mkdirs();
+						System.out.println("셋팅된 파일 전체 경로 : "+uploadFile);
+						item.write(uploadFile);//실제로 원하는 위치에 파일 쓰기
+						flist.add(new FileDTO(uploadFile));
+					}
+				}
+			}
+			request.setAttribute("file", flist);
+			request.setAttribute("user", user);
 		} catch (FileUploadException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		request.getRequestDispatcher("file_result.jsp").forward(request, response);
 	}
 
 	/**
