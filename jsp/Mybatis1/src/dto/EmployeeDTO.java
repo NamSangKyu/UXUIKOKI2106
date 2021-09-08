55b58f8ab9391471a56eeceb32d7850ab3ea93dd
NamSangKyu <nam2626@gmail.com>
2021-09-08 16:24:16 +0900

    MyBatis 예제

---
+++ jsp/Mybatis1/src/dto/EmployeeDTO.java
@@ -0,0 +1,53 @@
+package dto;
+
+public class EmployeeDTO {
+	private String eno;
+	private String name;
+	private String department;
+	private int position;
+	
+	public EmployeeDTO() {
+	}
+
+	public EmployeeDTO(String eno, String name, String department, int position) {
+		super();
+		this.eno = eno;
+		this.name = name;
+		this.department = department;
+		this.position = position;
+	}
+
+	public String getEno() {
+		return eno;
+	}
+
+	public void setEno(String eno) {
+		this.eno = eno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+	
+}
