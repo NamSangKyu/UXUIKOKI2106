c63d001138e803836ad6892b7ef4a9f39041d9d3
NamSangKyu <nam2626@gmail.com>
2021-09-10 17:23:51 +0900

    json 파일 읽기

--- jsp/MemberAjaxProject/src/test/java/test/TestMember.java
+++ jsp/MemberAjaxProject/src/test/java/test/TestMember.java
@@ -1,32 +1,58 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import dto.MemberDTO;
 
 class TestMember {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		//테스트용 데이터 읽기
 		//1. JSON 파일 읽기
+		FileReader file = new FileReader("member.json");
+		BufferedReader br = new BufferedReader(file);
+		String txt = new String();
+		
+		while(true) {
+			String temp = br.readLine();
+			if(temp == null) break;
+			txt += temp;
+		}
+		//System.out.println(txt);
+		JSONObject json = new JSONObject(txt);
+		JSONArray array = new JSONArray(json.getJSONArray("data"));
 		//2. 읽은 내용을 ArrayList<MemberDTO>에 저장
-		//3. 리스트 내용을 출력		
+		ArrayList<MemberDTO> list = new ArrayList<MemberDTO>();
+		for(int i=0;i<array.length();i++) {
+				JSONObject j = array.getJSONObject(i);
+				System.out.println(j);
+				String id = j.getString("id");
+				String passwd = j.getString("passwd");
+				String name = j.getString("name");
+				int age = j.getInt("age");
+				String grade = j.getString("grade");
+				String gender = j.getString("gender");
+				int point = j.getInt("point");
+				list.add(new MemberDTO(id, passwd, name, age, gender, point, grade));
+				
+		}	
+		//3.데이터 추가
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		//테스트 끝나면 롤백
 	}
 
 	@Test
