cd4c712a4980ad75b1cddee2cf2ff8c8afb0fd69
NamSangKyu <nam2626@gmail.com>
2021-09-14 14:50:05 +0900

    spring mvc

--- spring/SpringMVC/src/main/java/org/korea/MainController.java
+++ spring/SpringMVC/src/main/java/org/korea/MainController.java
@@ -2,6 +2,7 @@ package org.korea;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,4 +12,22 @@ public class MainController {
 		System.out.println("메인 페이지로 이동");
 		return "main";
 	}
+
+	@RequestMapping("/login.do")
+	public String login(@RequestParam("id")String id, @RequestParam("pass")String pass ) {
+		System.out.println(id + " " + pass);
+		return "main";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
