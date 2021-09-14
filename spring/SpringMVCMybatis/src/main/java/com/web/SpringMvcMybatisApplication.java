552b462627c572ca627b77d2602b6f0d88179a38
NamSangKyu <nam2626@gmail.com>
2021-09-14 12:46:30 +0900

    spring mvc, mybatis

---
+++ spring/SpringMVCMybatis/src/main/java/com/web/SpringMvcMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMybatisApplication.class, args);
+	}
+
+}
