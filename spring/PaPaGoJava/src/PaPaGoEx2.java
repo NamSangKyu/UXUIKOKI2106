37b85357e384c2a994a51918174c9a34bbeb4fd5
NamSangKyu <nam2626@gmail.com>
2021-09-17 16:22:18 +0900

    파파고 연동

--- spring/PaPaGoJava/src/PaPaGoEx2.java
+++ spring/PaPaGoJava/src/PaPaGoEx2.java
@@ -17,11 +17,15 @@ public class PaPaGoEx2 {
 	public static String papagoTranslate(String target, String text) {
 		String clientId = "_1rIIr0u6hwdD4VpqYnd";//애플리케이션 클라이언트 아이디값";
 		String clientSecret = "k5ERzutCdQ";//애플리케이션 클라이언트 시크릿값";
+		String msg = null;
+		HttpURLConnection con = null;
+		DataOutputStream dos = null;
+		BufferedReader br = null;
 		try {
 			text = URLEncoder.encode(text, "UTF-8");
 			//해당 주소 url 연결
 			URL url = new URL("https://openapi.naver.com/v1/papago/n2mt");
-	        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	        con = (HttpURLConnection) url.openConnection();
 	        con.setRequestMethod("POST");
 	        con.setRequestProperty("X-Naver-Client-Id", clientId);
 	        con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
@@ -29,87 +33,42 @@ public class PaPaGoEx2 {
 	        String postParams = "source=ko&target="+target+"&text=" + text;
 	        //데이터 전송
 	        con.setDoOutput(true);
-	        DataOutputStream dos = new DataOutputStream(con.getOutputStream());
+	        dos = new DataOutputStream(con.getOutputStream());
 	        dos.writeBytes(postParams);
 	        dos.flush();
 	        dos.close();
 	        //데이터 받는 부분
+	        int responseCode = con.getResponseCode();
+	        if (responseCode == HttpURLConnection.HTTP_OK) { 
+	        	br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	        }else {
+	        	br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	        }
+	        msg = new String();
+	        while(true) {
+	        	String str = br.readLine();
+	        	if(str == null) break;
+	        	msg += str;
+	        }
+	        JSONObject json = new JSONObject(msg);
+	        msg = json.getJSONObject("message").getJSONObject("result").getString("translatedText");
 		}catch (UnsupportedEncodingException e) {
 			throw new RuntimeException("인코딩 실패", e);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally {
+			
 		}
 		
-		return null;
+		return msg;
 	}
 	public static void main(String[] args) {
-
-        Map<String, String> requestHeaders = new HashMap<>();
-       
-
-        String responseBody = post(apiURL, requestHeaders, text);
-        JSONObject json = new JSONObject(responseBody);
-        String resultMessage =
-        		json.getJSONObject("message").getJSONObject("result").getString("translatedText");
-        System.out.println(resultMessage);
+		String result = papagoTranslate("ja", "일본에 태풍 또 가네");
+		System.out.println(result);
+   
     }
 
-    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
-        HttpURLConnection con = connect(apiUrl);
-        String postParams = "source=ko&target=en&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
-        try {
-            
-            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
-                con.setRequestProperty(header.getKey(), header.getValue());
-            }
-
-            con.setDoOutput(true);
-            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-                wr.write(postParams.getBytes());
-                wr.flush();
-            }
-
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
-                return readBody(con.getInputStream());
-            } else {  // 에러 응답
-                return readBody(con.getErrorStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        } finally {
-            con.disconnect();
-        }
-    }
-
-    private static HttpURLConnection connect(String apiUrl){
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection)url.openConnection();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
-        } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
-        }
-    }
-
-    private static String readBody(InputStream body){
-        InputStreamReader streamReader = new InputStreamReader(body);
-
-        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
-            StringBuilder responseBody = new StringBuilder();
-
-            String line;
-            while ((line = lineReader.readLine()) != null) {
-                responseBody.append(line);
-            }
-
-            return responseBody.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
-        }
-    }
-
+   
 }
