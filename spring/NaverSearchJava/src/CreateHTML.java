d198e1e491fc2f1a447c988fbe7d964a2fc06c06
NamSangKyu <nam2626@gmail.com>
2021-09-23 15:41:29 +0900

    html 파일 읽기

--- spring/NaverSearchJava/src/CreateHTML.java
+++ spring/NaverSearchJava/src/CreateHTML.java
@@ -1,10 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CreateHTML {
-
+	public static String readHTML() {
+		String result = "";
+		//파일 읽기 처리
+		//template.html 파일 내용을 전부 result에 저장
+		//FileReader, BufferedReader
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("template.html");
+			br = new BufferedReader(fr);
+			String temp;
+			while((temp = br.readLine()) != null) {
+				result += temp;
+			}
+			System.out.println(result);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br!=null)br.close();
+				if(fr!=null)fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 	public static void main(String[] args) {
-		//파일 쓰기
-		String tag = "<!DOCTYPE html><html><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><title>Page Title</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body></body></html>";
-	
+		readHTML();
 	}
 
 }
+
+
