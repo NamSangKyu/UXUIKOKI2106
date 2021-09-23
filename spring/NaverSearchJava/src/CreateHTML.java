f3e3b5d43320184f72edab98056fb9b8cfbbc52f
NamSangKyu <nam2626@gmail.com>
2021-09-23 15:21:10 +0900

    네이버 검색 & 파일출력

---
+++ spring/NaverSearchJava/src/CreateHTML.java
@@ -0,0 +1,10 @@
+
+public class CreateHTML {
+
+	public static void main(String[] args) {
+		//파일 쓰기
+		String tag = "<!DOCTYPE html><html><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><title>Page Title</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body></body></html>";
+	
+	}
+
+}
