04b7df3c0974bacf40a40531c6a32c294ee75f67
NamSangKyu <nam2626@gmail.com>
2021-09-27 10:11:05 +0900

    날씨 API

--- spring/WeatherTest/src/WeatherEx.java
+++ spring/WeatherTest/src/WeatherEx.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WeatherEx {
 
@@ -23,7 +29,32 @@ public class WeatherEx {
 		System.out.println(apiUrl);
 		
 		//URL 호출해서 받은 내용 전체 출력
-		
+		try {
+			URL u = new URL(apiUrl);
+			HttpURLConnection connection = (HttpURLConnection) u.openConnection();
+			connection.setRequestMethod("GET");
+			String result = "";
+			
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(connection.getInputStream()));
+			String temp;
+			while((temp = br.readLine())!=null) {
+				result += temp;
+			}
+			System.out.println(result);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
