a83d84e3f07fa8d9b3f7062e56b6e35471a16096
NamSangKyu <nam2626@gmail.,com>
2021-09-29 17:50:51 +0900

    안드로이드 뷰 예제

--- android/widget/app/src/main/java/com/example/widget/MainActivity.java
+++ android/widget/app/src/main/java/com/example/widget/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.widget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                //이동할 액티비티 선택, 현재 액티비티, 이동할 액티비티 설정
+                intent.setClass(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
