0e6a9227e18c210aefa4a56d7f18d4471a56dbe0
NamSangKyu <nam2626@gmail.,com>
2021-09-29 16:40:50 +0900

    안드로이드 뷰 예제

---
+++ android/widget/app/src/main/java/com/example/widget/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
