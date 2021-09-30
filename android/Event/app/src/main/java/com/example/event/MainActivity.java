d85901207bfa546b1b763568e258eef35dfecfdf
NamSangKyu <nam2626@gmail.,com>
2021-09-30 09:44:46 +0900

    이벤트 예제

---
+++ android/Event/app/src/main/java/com/example/event/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
