96ff14d63e92303ea8c11bf00da882604ecd700b
NamSangKyu <nam2626@gmail.,com>
2021-09-30 12:07:04 +0900

    라이프사이클 예제

--- android/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
+++ android/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.lifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LIFE", "onCreate");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Data Setting");
+        EditText editText = findViewById(R.id.edt_txt);
+        Button btn = findViewById(R.id.button);
+
+        if(savedInstanceState != null){
+            str = savedInstanceState.getString("str");
+            Toast.makeText(this,"Data Load",Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d("LIFE", "onCreate: "+str);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = editText.getText().toString();
+                Toast.makeText(MainActivity.this,str+" content save", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -42,5 +68,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d("LIFE", "onResume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("str",str);
     }
 }
