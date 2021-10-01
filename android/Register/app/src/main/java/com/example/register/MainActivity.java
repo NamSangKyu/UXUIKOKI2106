8f5350c4b783501df281e7cab637606938649716
NamSangKyu <nam2626@gmail.,com>
2021-10-01 10:49:27 +0900

    안드로이드 액티비티 이동 예제

--- android/Register/app/src/main/java/com/example/register/MainActivity.java
+++ android/Register/app/src/main/java/com/example/register/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,13 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         EditText txtId = findViewById(R.id.txt_id);
         EditText txtPass = findViewById(R.id.txt_password);
-
+        CheckBox box = findViewById(R.id.checkBox);
         Button btn = findViewById(R.id.btn_login);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String id = txtId.getText().toString();
                 String pass = txtPass.getText().toString();
+                //isChecked로 체크박스가 체크가 되어 있는지 안되어 있는지 체크
+                Toast.makeText(getBaseContext(),String.valueOf(box.isChecked()),Toast.LENGTH_SHORT).show();
                 if(id.equals("koreait") && pass.equals("123456")){
                     Intent intent = new Intent(MainActivity.this,BoardListActivity.class);
                     startActivity(intent);
