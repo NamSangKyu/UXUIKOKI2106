c117416257ebcef4c3dce60413ebe067b1d65aaf
NamSangKyu <nam2626@gmail.,com>
2021-10-06 09:17:30 +0900

    Custom ListView 예제

--- android/CustomListView/app/src/main/java/com/example/customlistview/MainActivity.java
+++ android/CustomListView/app/src/main/java/com/example/customlistview/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.customlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
         customListAdapter.addItem(getDrawable(R.drawable.cloud_sun),"구름/해");
 
         view.setAdapter(customListAdapter);
+
+        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ListViewItem item = (ListViewItem) customListAdapter.getItem(i);
+                Toast.makeText(MainActivity.this,item.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
