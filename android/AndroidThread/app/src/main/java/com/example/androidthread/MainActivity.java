ccc6a28beded631236287de7c22a79270715e386
NamSangKyu <nam2626@gmail.,com>
2021-10-06 15:14:13 +0900

    Handler 예제

--- android/AndroidThread/app/src/main/java/com/example/androidthread/MainActivity.java
+++ android/AndroidThread/app/src/main/java/com/example/androidthread/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidthread;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button button;
     int count;
+    NumberHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         count = 0;
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
-
+        handler = new NumberHandler();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,8 +41,35 @@ public class MainActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                textView.setText("count : " + count);
+                //핸들러로 전달할 메세지
+                Message msg = handler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("count",count);
+                msg.setData(bundle);
+                handler.sendMessage(msg);
             }
         }
     }
+    class NumberHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            int count = bundle.getInt("count");
+            textView.setText("count : " + count);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
