6df7dcf64ca3546f22c90b6710ff1c50d67c0fce
NamSangKyu <nam2626@gmail.,com>
2021-10-07 17:07:16 +0900

    SQLite 기본

--- android/db/app/src/main/java/com/example/db/MemberRegisterActivity.java
+++ android/db/app/src/main/java/com/example/db/MemberRegisterActivity.java
@@ -3,12 +3,37 @@ package com.example.db;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MemberRegisterActivity extends AppCompatActivity {
+    private TextView txtName;
+    private TextView txtTel;
+    private DBHelper dbHelper;
+    private Button btnAdd;
+    private Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_register);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, tel;
+                name = txtName.getText().toString();
+                tel = txtTel.getText().toString();
+                dbHelper.add(new AddressVO(name, tel));
+                finishActivity(1000);
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
