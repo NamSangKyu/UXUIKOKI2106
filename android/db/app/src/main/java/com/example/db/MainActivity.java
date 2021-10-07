7747d2d5b57e47e2f400fd176c9c878e7917dd2e
NamSangKyu <nam2626@gmail.,com>
2021-10-07 16:48:56 +0900

    SQLite 기본

--- android/db/app/src/main/java/com/example/db/MainActivity.java
+++ android/db/app/src/main/java/com/example/db/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.db;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = findViewById(R.id.main_btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MemberRegisterActivity.class);
+                startActivityForResult(intent,1000);
+            }
+        });
     }
 }
---
+++ android/db/app/src/main/java/com/example/db/MemberRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_register);
+    }
+}
