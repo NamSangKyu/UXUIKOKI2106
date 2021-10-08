fcd2d839d5b2b245fb240a966519740166854813
NamSangKyu <nam2626@gmail.,com>
2021-10-08 14:41:01 +0900

    날씨

--- android/Weather/app/src/main/java/com/example/weather/MainActivity.java
+++ android/Weather/app/src/main/java/com/example/weather/MainActivity.java
@@ -5,24 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     Weather weather;
+    TextView detail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        detail = findViewById(R.id.detail);
         weather = new Weather();
         weather.execute();
+
     }
     class Weather extends AsyncTask<String,Void,String>{
         int timeList[] = new int[]{2,5,8,11,14,17,20,23};
@@ -47,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             baseDate = sdf.format(Calendar.getInstance().getTime());
             dataType = "json";
             baseTime = getTime();
-            numOfRows = "20";
+            numOfRows = "14";
             nx = "60";
             ny="127";
             apiURL += "?serviceKey="+serviceKey;
@@ -58,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             apiURL += "&ny="+ny;
             apiURL += "&numOfRows="+numOfRows;
             URL url = null;
+            JSONObject r = new JSONObject();
             try {
                 url = new URL(apiURL);
                 Log.i("result", "doInBackground: "+url.toString());
@@ -72,13 +83,53 @@ public class MainActivity extends AppCompatActivity {
                     result += line;
 
                 Log.i("result", "doInBackground: "+result);
+                JSONObject json = new JSONObject(result);
+                //reponse -> body -> items -> item -> 배열
+                JSONArray arr = json.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+                for(int i=0;i<arr.length();i++) {
+                    //Log.i("result", "doInBackground: "+arr.get(i));
+                    JSONObject obj = (JSONObject) arr.get(i);
+
+                    if (obj.getString("category").equals("TMP")) {
+                        Log.i("result", obj.getString("category") + " : " + obj.getString("fcstValue"));
+                        r.put(obj.getString("category"), obj.getString("fcstValue"));
+                    }else if(obj.getString("category").equals("PTY")) {
+                        Log.i("result", obj.getString("category") + " : " + obj.getString("fcstValue"));
+                        r.put(obj.getString("category"), obj.getString("fcstValue"));
+                    }else if(obj.getString("category").equals("REH")) {
+                        Log.i("result", obj.getString("category") + " : " + obj.getString("fcstValue"));
+                        r.put(obj.getString("category"), obj.getString("fcstValue"));
+                    }else if(obj.getString("category").equals("SKY")) {
+                        Log.i("result", obj.getString("category") + " : " + obj.getString("fcstValue"));
+                        r.put(obj.getString("category"), obj.getString("fcstValue"));
+                    }
+                    }
+                } catch (ProtocolException protocolException) {
+                protocolException.printStackTrace();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (JSONException jsonException) {
+                jsonException.printStackTrace();
+            }
+            return r.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject json = new JSONObject(s);
+                String msg = "온도 : "+json.getString("TMP") +"\n" +
+                        "강수형태 : "+json.getString("PTY") +"\n" +
+                        "하늘상태 : "+json.getString("SKY") +"\n" +
+                        "습도 : "+json.getString("REH") +"\n";
+                
+                detail.setText(msg);
+                        
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return null;
         }
     }
 }
