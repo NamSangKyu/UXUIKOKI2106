24aa999adf0ed3716561c1887d22dcc121f24b14
NamSangKyu <nam2626@gmail.,com>
2021-10-13 14:35:13 +0900

    Glide 예제

--- android/Glide/app/src/main/java/com/example/glide/MainActivity.java
+++ android/Glide/app/src/main/java/com/example/glide/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ListView list = findViewById(R.id.listView);
         adapter = new CustomListAdapter();
-        adapter.AddItem("https://pbs.twimg.com/media/EviLd44UcAU4_lw.jpg","제목1","내용1");
-        adapter.AddItem("https://pbs.twimg.com/media/EviLd44UcAU4_lw.jpg1","제목2","내용2");
-        adapter.AddItem("https://pbs.twimg.com/media/EviLd44UcAU4_lw.jpg2","제목3","내용3");
-        adapter.AddItem("https://t1.daumcdn.net/cfile/tistory/99B75C495C5402CF35","제목4","내용4");
+        adapter.AddItem("이미지 URL","제목1","내용1");
+        adapter.AddItem("이미지 URL","제목2","내용2");
+        adapter.AddItem("이미지 URL","제목3","내용3");
+        adapter.AddItem("이미지 URL","제목4","내용4");
         list.setAdapter(adapter);
 
     }
